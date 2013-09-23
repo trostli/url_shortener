1d54c5574edd20a81449e5a1ed058251eef054e2
Daniel Trostli <daniel.trostli@gmail.com>
2013-09-22 19:35:46 -0700

    Added http validation

--- models/url.rb
+++ models/url.rb
@@ -1,3 +1,10 @@
 class URL < ActiveRecord::Base
-	validates :url, presence: true
+	validates :url, presence: true 
+	validate :include_http_or_https?
+
+	def include_http_or_https?
+		unless self.url.start_with?('http://', 'https://')
+			errors.add(:url, "has to include http:// or https://")
+		end
+	end
 end
